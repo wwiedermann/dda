ff52853feb34ba936a82d3026ffbed031f143365
Megan Hirni <163319503+hirnia@users.noreply.github.com>
2026-02-25 12:09:55 -0600

    Bagging function includes bag OLS coef
    There's now a print script for the OLS bagging that shows the estimate, 2.5% & 97.5% CI (will change to add flexibility later), so the bagging function was altered to include bagged target & alt standard coefficients.

--- R/dda_bagging.R
+++ R/dda_bagging.R
@@ -73,8 +73,6 @@ dda_bagging <- function(
     } else if (inherits(call_info$formula, "lm")) {
       original_formula <- formula(call_info$formula)
     } else if (!is.null(call_info$all_args$formula)) {
-
-
       if (inherits(call_info$all_args$formula, "formula")) {
         original_formula <- call_info$all_args$formula
       } else if (inherits(call_info$all_args$formula, "lm")) {
@@ -93,8 +91,21 @@ dda_bagging <- function(
   cov_names <- setdiff(all_vars, c(y_name, x_name))
   has_covariates <- length(cov_names) > 0
 
+  # --- Setup formulas for standard OLS aggregation ---
+  if (has_covariates) {
+    cov_str <- paste(cov_names, collapse = " + ")
+    full_formula_tar <- as.formula(paste(y_name, "~", x_name, "+", cov_str))
+    full_formula_alt <- as.formula(paste(x_name, "~", y_name, "+", cov_str))
+  } else {
+    full_formula_tar <- as.formula(paste(y_name, "~", x_name))
+    full_formula_alt <- as.formula(paste(x_name, "~", y_name))
+  }
+
   # --- Bootstrap Execution Loop ---
   bagged_results <- vector("list", iter)
+  ols_tar_coefs <- vector("list", iter)
+  ols_alt_coefs <- vector("list", iter)
+
   if (progress) pb <- txtProgressBar(min = 0, max = iter, style = 3)
 
   for(i in 1:iter) {
@@ -103,15 +114,18 @@ dda_bagging <- function(
     boot_indices <- sample(1:nobs, nobs, replace = TRUE)
     datboot <- original_data[boot_indices, ]
 
-    # 2. Residualize Variables
+    # 2. Fit Standard OLS Models for Aggregation
+    lm_tar <- tryCatch(lm(full_formula_tar, data = datboot), error = function(e) NULL)
+    lm_alt <- tryCatch(lm(full_formula_alt, data = datboot), error = function(e) NULL)
+
+    if (!is.null(lm_tar)) ols_tar_coefs[[i]] <- coef(lm_tar)
+    if (!is.null(lm_alt)) ols_alt_coefs[[i]] <- coef(lm_alt)
+
+    # 3. Residualize Variables for DDA
     if (has_covariates) {
       cov_formula_y <- as.formula(paste(y_name, "~", paste(cov_names, collapse = " + ")))
       cov_formula_x <- as.formula(paste(x_name, "~", paste(cov_names, collapse = " + ")))
-      # To consider: run competing models to create using lm
-      #tar <-
-      #alt <-
       tryCatch({
-
         ry <- as.vector(scale(resid(lm(cov_formula_y, data = datboot))))
         rx <- as.vector(scale(resid(lm(cov_formula_x, data = datboot))))
       }, error = function(e) {
@@ -122,7 +136,7 @@ dda_bagging <- function(
       rx <- as.vector(scale(datboot[[x_name]]))
     }
 
-    # 3. boot_args obj
+    # 4. boot_args obj
     boot_args <- call_info$all_args
     if (length(boot_args) > 0 && names(boot_args)[1] == "") {
       boot_args[[1]] <- NULL # remove the original lm object
@@ -135,7 +149,7 @@ dda_bagging <- function(
     names(boot_processed) <- c(x_name, y_name)
     boot_args$data <- boot_processed
 
-    # 4. Call DDA Module
+    # 5. Call DDA Module
     bagged_results[[i]] <- tryCatch(
       do.call(dda_func, boot_args),
       error = function(e) {
@@ -150,7 +164,8 @@ dda_bagging <- function(
   if (progress) close(pb)
 
   # --- Filter Valid Results ---
-  valid_res <- bagged_results[!sapply(bagged_results, function(x) all(is.na(x)) || is.null(x))]
+  is_valid <- !sapply(bagged_results, function(x) all(is.na(x)) || is.null(x))
+  valid_res <- bagged_results[is_valid]
   n_valid <- length(valid_res)
 
   if (n_valid == 0) {
@@ -160,6 +175,33 @@ dda_bagging <- function(
   agg <- list()
   decs <- list()
   crit <- qnorm(1 - alpha/2)
+
+  # --- Aggregate OLS Results (confint style) ---
+  agg$ols_target <- tryCatch({
+    mat <- do.call(rbind, ols_tar_coefs[is_valid])
+    lb <- alpha / 2
+    ub <- 1 - (alpha / 2)
+    out_mat <- cbind(
+      estimate = colMeans(mat, na.rm = TRUE),
+      apply(mat, 2, quantile, probs = lb, na.rm = TRUE),
+      apply(mat, 2, quantile, probs = ub, na.rm = TRUE)
+    )
+    colnames(out_mat) <- c("estimate", paste0(lb * 100, " %"), paste0(ub * 100, " %"))
+    out_mat
+  }, error = function(e) NULL)
+
+  agg$ols_alternative <- tryCatch({
+    mat <- do.call(rbind, ols_alt_coefs[is_valid])
+    lb <- alpha / 2
+    ub <- 1 - (alpha / 2)
+    out_mat <- cbind(
+      estimate = colMeans(mat, na.rm = TRUE),
+      apply(mat, 2, quantile, probs = lb, na.rm = TRUE),
+      apply(mat, 2, quantile, probs = ub, na.rm = TRUE)
+    )
+    colnames(out_mat) <- c("estimate", paste0(lb * 100, " %"), paste0(ub * 100, " %"))
+    out_mat
+  }, error = function(e) NULL)
 
   ## ============================================================================
   ## INDEP Block
@@ -268,6 +310,7 @@ dda_bagging <- function(
   ## ============================================================================
   if (obj_type == "dda.resdist") {
     agg$var.names <- var_names
+    prob_trans_flag <- if(!is.null(dda_result$probtrans)) isTRUE(dda_result$probtrans) else FALSE
 
     z_tar <- sapply(valid_res, function(x) get_numeric(x$agostino$target$statistic[2]))
     z_alt <- sapply(valid_res, function(x) get_numeric(x$agostino$alternative$statistic[2]))
@@ -304,8 +347,16 @@ dda_bagging <- function(
       nc <- ncol(mat)
       low_idx <- nc - 1
       upp_idx <- nc
-      decs[[paste0("dec_", k)]] <- calc_props(ifelse(mat[,low_idx] > 0 & mat[,upp_idx] > 0, "Target",
-                                                     ifelse(mat[,low_idx] < 0 & mat[,upp_idx] < 0, "Alternative", "Undecided")))
+
+      is_reversed <- prob_trans_flag && (k %in% c("skewdiff", "kurtdiff"))
+
+      if (is_reversed) {
+        decs[[paste0("dec_", k)]] <- calc_props(ifelse(mat[,low_idx] < 0 & mat[,upp_idx] < 0, "Target",
+                                                       ifelse(mat[,low_idx] > 0 & mat[,upp_idx] > 0, "Alternative", "Undecided")))
+      } else {
+        decs[[paste0("dec_", k)]] <- calc_props(ifelse(mat[,low_idx] > 0 & mat[,upp_idx] > 0, "Target",
+                                                       ifelse(mat[,low_idx] < 0 & mat[,upp_idx] < 0, "Alternative", "Undecided")))
+      }
     }
   }
 
