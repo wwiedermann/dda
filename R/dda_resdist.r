ebfa87b3ab63a78dd01c2d37d0e03e0ee7099b6d
Megan Hirni <163319503+hirnia@users.noreply.github.com>
2026-04-09 10:18:59 -0500

    WW's Documentation Changes

--- R/dda_resdist.r
+++ R/dda_resdist.r
@@ -1,27 +1,40 @@
 #' @title Direction Dependence Analysis: Residual Distributions
+#'
 #' @description \code{dda.resdist} evaluates patterns of asymmetry of error
-#'              distributions of causally competing models (\code{y ~ x}
-#'              vs. \code{x ~ y}).
+#' distributions of causally competing models (\code{y ~ x} vs. \code{x ~ y}).
+#' \code{print} returns DDA test statistics associated with \code{dda.resdist}
+#' objects.
+#'
 #' @name dda.resdist
 #'
-#' @param formula     Symbolic formula of the target model to be tested or a \code{lm} object.
-#' @param pred        Variable name of the predictor which serves as the outcome in the alternative model.
-#' @param data        An optional data frame containing the variables in the
-#'                    model (by default variables are taken from the environment
-#'                    which \code{dda.resdist} is called from).
-#' @param B           Number of bootstrap samples.
-#' @param boot.type   A vector of character strings representing the type of
-#'                    bootstrap confidence intervals required. Must be one of
-#'                    the two values \code{c("perc", "bca")};
-#'                    \code{boot.type = "perc"} is the default.
-#' @param prob.trans  A logical value indicating whether a probability integral
-#'                    transformation should be performed prior computation of
-#'                    skewness and kurtosis difference tests.
-#' @param conf.level  Confidence level for bootstrap confidence intervals.
-#' @param ...         Additional arguments to be passed to the function.
+#' @param formula Symbolic formula of the target model to be tested or an
+#'   \code{lm} object.
+#' @param pred Variable name of the predictor which serves as the outcome in
+#'   the alternative model.
+#' @param data An optional data frame containing the variables in the model
+#'   (by default variables are taken from the environment which
+#'   \code{dda.resdist} is called from).
+#' @param B Number of bootstrap samples.
+#' @param boot.type A vector of character strings representing the type of
+#'   bootstrap confidence intervals. Must be one of the two values
+#'   \code{c("perc", "bca")}; \code{boot.type = "perc"} is the default.
+#' @param prob.trans A logical value indicating whether a probability integral
+#'   transformation should be performed prior to computation of skewness and
+#'   kurtosis tests.
+#' @param conf.level Confidence level for bootstrap confidence intervals.
+#' @param robust A logical value indicating whether Siegel's (1982) repeated
+#'   median estimator should be used for model estimation. If
+#'   \code{robust = TRUE} repeated median estimation is applied for the
+#'   causally competing models, otherwise ordinary least squares (OLS)
+#'   estimation is used.
+#' @param ... Additional arguments to be passed to the function.
 #'
-#' @returns An object of class \code{ddaresdist} containing the results of
-#'          direction dependence tests of error distributions.
+#' @return An object of class \code{dda.resdist} containing the results of
+#'   direction dependence tests of error distributions.
+#'
+#' @references Wiedermann, W., & von Eye, A. (2025). \emph{Direction
+#'   Dependence Analysis: Foundations and Statistical Methods}. Cambridge, UK:
+#'   Cambridge University Press.
 #'
 #' @examples
 #' set.seed(123)
@@ -32,9 +45,10 @@
 #' d <- data.frame(x, y)
 #'
 #' result <- dda.resdist(y ~ x, pred = "x", data = d,
-#'             B = 50, conf.level = 0.90, prob.trans = TRUE)
+#'   B = 50, conf.level = 0.90, prob.trans = TRUE)
 #'
-#' @references Wiedermann, W., & von Eye, A. (2025). Direction Dependence Analysis: Foundations and Statistical Methods. Cambridge, UK: Cambridge University Press.
+#' print(result)
+#'
 #' @export
 #' @rdname dda.resdist
 dda.resdist <- function(formula,
@@ -381,7 +395,8 @@ dda.resdist <- function(formula,
 #' @name print.dda.resdist
 #' @title Print Method for \code{dda.resdist} Objects
 #'
-#' @description \code{print} returns DDA test statistics associated with \code{dda.resdist} objects.
+#' @description \code{print} returns DDA test statistics associated with
+#'   \code{dda.resdist} objects.
 #'
 #' @param x An object of class \code{dda.resdist} when using \code{print}.
 #' @param ... Additional arguments to be passed to the method.
